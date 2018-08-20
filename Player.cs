e67a4cc0c4c36da25c0d5918b65007a97bdebcc4
LeksyCode <nikolaev.profent@gmail.com>
2018-08-20 18:39:11 +0300

    added the ability to create a player's field

--- Player.cs
+++ Player.cs
@@ -10,14 +10,16 @@ namespace Sea_Wars
     class Player
     {
         string Name { get; set; }
-        int[] coordinatesX = new int[20], coordinatesY = new int[20];
+
+        private int currentBoats = 0;
+        private bool verticall = false;
 
         public Player(string name)
         {
             Name = name;
         }
 
-        public void CreatingBoats()
+        public void CreatingPlayerMap()
         {
             int currentCell_X = 2, currentCell_Y = 1;
             bool fixSharp = false;
@@ -25,13 +27,20 @@ namespace Sea_Wars
             Console.SetCursorPosition(currentCell_X, currentCell_Y);
             Console.Write("@");
 
-            int currentBoats = 0;
-            int stage = 0;
+
+            int stage = 4;
 
             while (currentBoats <= 19)
             {
-                
-                GameEngine.ShowInfo(currentBoats, stage);
+                if (currentBoats >= 4 && currentBoats < 10)
+                    stage = 3;
+                else if (currentBoats >= 10 && currentBoats < 16)
+                    stage = 2;
+                else if (currentBoats >= 16)
+                    stage = 1;
+
+
+                GameEngine.ShowInfo(stage);
 
                 ConsoleKey direction = Console.ReadKey(true).Key;
 
@@ -39,6 +48,7 @@ namespace Sea_Wars
                 {
                     if (currentCell_X > 2)
                     {
+                        GameEngine.DrawPlayerMap();
                         if (!fixSharp)
                         {
                             Console.SetCursorPosition(currentCell_X, currentCell_Y);
@@ -48,7 +58,6 @@ namespace Sea_Wars
                         Console.SetCursorPosition(currentCell_X, currentCell_Y);
                         Console.Write("@");
                         fixSharp = false;
-                        stage = 0;
                     }
 
                 }
@@ -56,6 +65,7 @@ namespace Sea_Wars
                 {
                     if (currentCell_X < 20)
                     {
+                        GameEngine.DrawPlayerMap();
                         if (!fixSharp)
                         {
                             Console.SetCursorPosition(currentCell_X, currentCell_Y);
@@ -64,14 +74,15 @@ namespace Sea_Wars
                         currentCell_X += 2;
                         Console.SetCursorPosition(currentCell_X, currentCell_Y);
                         Console.Write("@");
+                        fixSharp = false;
                     }
-                    fixSharp = false;
-                    stage = 0;
+
                 }
                 else if (direction == ConsoleKey.UpArrow || direction == ConsoleKey.W)
                 {
                     if (currentCell_Y > 1)
                     {
+                        GameEngine.DrawPlayerMap();
                         if (!fixSharp)
                         {
                             Console.SetCursorPosition(currentCell_X, currentCell_Y);
@@ -80,14 +91,14 @@ namespace Sea_Wars
                         currentCell_Y--;
                         Console.SetCursorPosition(currentCell_X, currentCell_Y);
                         Console.Write("@");
+                        fixSharp = false;
                     }
-                    fixSharp = false;
-                    stage = 0;
                 }
                 else if (direction == ConsoleKey.DownArrow || direction == ConsoleKey.S)
                 {
                     if (currentCell_Y < 9)
                     {
+                        GameEngine.DrawPlayerMap();
                         if (!fixSharp)
                         {
                             Console.SetCursorPosition(currentCell_X, currentCell_Y);
@@ -96,56 +107,107 @@ namespace Sea_Wars
                         currentCell_Y++;
                         Console.SetCursorPosition(currentCell_X, currentCell_Y);
                         Console.Write("@");
+                        fixSharp = false;
                     }
-                    fixSharp = false;
-                    stage = 0;
                 }
                 else if (direction == ConsoleKey.Enter)
                 {
-                        if (CheckBesideCell(currentCell_X, currentCell_Y, currentBoats, out int phaze))
-                        {
-
-                        stage = phaze;                        
-                        continue;
-                        }                        
                     fixSharp = true;
-                    Console.SetCursorPosition(currentCell_X, currentCell_Y);
-                    Console.Write('#');
-                    coordinatesX[currentBoats] = currentCell_X;
-                    coordinatesY[currentBoats] = currentCell_Y;
-                    Board.PlayerField[coordinatesY[currentBoats]] = Program.ChengeSymb(coordinatesX[currentBoats], Board.PlayerField[coordinatesY[currentBoats]], '#');
-                    currentBoats++;
-                    GameEngine.DrawPlayerMap();
+                    SetShip(stage, currentCell_X, currentCell_Y, false);
+
                 }
             }
         }
 
-
-        public bool CheckBesideCell(int x, int y, int countShips, out int phaze)
+        public void SetShip(int deck, int x, int y, bool verticall)
         {
-            phaze = 4;
+            int canSetRight = 0, canSetLeft = 0;
+            bool locationRight = false, locationUp = false;
+
+            if (x < (Board.width / 2) + 2)
+            {
+                locationRight = true;
+            }
+            if (y > (9 / 2))
+            {
+                locationUp = true;
+            }
+
+
+            if (locationRight == true)
+            {
+                for (int i = 0; i < deck; i++)
+                {
+                    if (CheckBesideCell(x + (i * 2), y))
+                    {
+                        canSetRight++;
+                    }
+                }
+            }
+            else if (locationRight == false)
+            {
+                for (int i = 0; i < deck; i++)
+                {
+                    if (CheckBesideCell(x - (i * 2), y))
+                    {
+                        canSetLeft++;
+                    }
+                }
+            }
+
+            if (canSetRight == deck)
+            {
+                for (int i = 0; i < deck; i++)
+                {
+                    Console.SetCursorPosition(x + (i * 2), y);
+                    Console.Write('#');
+                    Board.PlayerField[y] = Program.ChengeSymb(x + (i * 2), Board.PlayerField[y], '#');
+                    currentBoats++;
+                }
+            }
+            else if (canSetLeft == deck)
+            {
+                for (int i = 0; i < deck; i++)
+                {
+                    Console.SetCursorPosition(x - (i * 2), y);
+                    Console.Write('#');
+                    Board.PlayerField[y] = Program.ChengeSymb(x - (i * 2), Board.PlayerField[y], '#');
+                    currentBoats++;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+
+
+        public bool CheckBesideCell(int x, int y)
+        {
             if (Board.PlayerField[y][x] == '#')
             {
-                return true;
+                return false;
             }
             else if (Board.PlayerField[y + 1][x] == '#' || Board.PlayerField[y - 1][x] == '#')
             {
-                return true;
+                return false;
             }
             else if (Board.PlayerField[y][x - 2] == '#' || Board.PlayerField[y][x + 2] == '#')
             {
-                return true;
+                return false;
             }
             else if (Board.PlayerField[y + 1][x - 2] == '#' || Board.PlayerField[y + 1][x + 2] == '#')
             {
-                return true;
+                return false;
             }
             else if (Board.PlayerField[y - 1][x - 2] == '#' || Board.PlayerField[y - 1][x + 2] == '#')
             {
-                return true;
+                return false;
             }
-            return false;
+            return true;
         }
-       
+
     }
 }
